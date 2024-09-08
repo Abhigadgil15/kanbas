46d655d1dbacd81efdfa7baf0e045fe67f1da43e
Abhinav Gadgil <64949128+Abhigadgil15@users.noreply.github.com>
2024-09-08 17:25:14 -0400

    update app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< Updated upstream
         <h1> Hello!! This is my React App</h1>
+=======
+        <h1> Hello!! This is my Main</h1>
+>>>>>>> Stashed changes
       </header>
     </div>
   );
