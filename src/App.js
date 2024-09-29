6eab9ba2b5856abfc161bd239b38355c33e5bb05
Abhinav Gadgil <64949128+Abhigadgil15@users.noreply.github.com>
2024-09-29 14:16:15 -0400

    Revert "Update assi2"
    This reverts commit 911e466fac7684e89860f32b94ced9483ac88881.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-<<<<<<< Updated upstream
  import './App.css';
 
 function App() {
-=======
-import Labs from "./Labs";
-import Kanbas from "./Kanbas";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
-const App = () => {
->>>>>>> Stashed changes
   return (
     <div className="App">
       <header className="App-header">
