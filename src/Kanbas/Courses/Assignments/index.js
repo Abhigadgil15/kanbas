3dcc41a8975b5d83ad94a64c25b21d4d50a6e037
Abhinav Gadgil <64949128+Abhigadgil15@users.noreply.github.com>
2024-10-02 01:24:46 -0400

    in progress

--- src/Kanbas/Courses/Assignments/index.js
+++ src/Kanbas/Courses/Assignments/index.js
@@ -1,14 +1,34 @@
+import { BsGripVertical } from "react-icons/bs";
+import { FaPlus } from "react-icons/fa"; // Importing plus icon for buttons
+import { MdSearch } from "react-icons/md"; // Importing search icon for the search input
+import LessonControlButtons from "../Modules/LessonControlButton";
+
 export default function Assignments() {
   return (
-    <div id="wd-assignments">
-      <input id="wd-search-assignment" placeholder="Search for Assignments" />
-      <button id="wd-add-assignment-group">+ Group</button>
-      <button id="wd-add-assignment">+ Assignment</button>
-      <h3 id="wd-assignments-title">
-        ASSIGNMENTS 40% of Total <button>+</button>
+    <div>
+      <div className="input-group mb-3">
+        <input
+          id="wd-search-assignment"
+          placeholder="Search for Assignments"
+          className="form-control"
+        />
+        <span className="input-group-text">
+          <MdSearch />
+        </span>
+      </div>
+      <button id="wd-add-assignment-group" className="btn btn-primary me-1">
+        <FaPlus /> Group
+      </button>
+      <button id="wd-add-assignment" className="btn btn-success">
+        <FaPlus /> Assignment
+      </button>
+      <h3 id="wd-assignments-title" className="fs-5">
+        ASSIGNMENTS 40% of Total 
+        <button className="btn btn-secondary ms-2">+</button>
       </h3>
-      <ul id="wd-assignment-list">
-        <li className="wd-assignment-list-item">
+      <ul id="wd-assignment-list" className="list-group rounded-0">
+        <li className="wd-assignment-list-item list-group-item p-3 mb-3 border-gray">
+          <BsGripVertical className="me-2 fs-3" />
           <a
             className="wd-assignment-link"
             href="#/Kanbas/Courses/1234/Assignments/123"
@@ -16,64 +36,76 @@ export default function Assignments() {
             A1 - Binary Search Trees
           </a>
           <br />
-          Multiple Modules | <b>Not available until</b> June 8 at 12:00 am |{" "}
+          <span className="text-red" style={{ marginLeft: '35px'}}>Multiple Modules</span> | <b>Not available until</b> June 8 at 12:00 am |{" "}
           <b>Due</b> June 23 at 11:59 pm | 100 pts
+          <LessonControlButtons/>
         </li>
-        <li className="wd-assignment-list-item">
+        <li className="wd-assignment-list-item list-group-item p-3 mb-3 border-gray">
+          <BsGripVertical className="me-2 fs-3" />
           <a
             className="wd-assignment-link"
-            href="#/Kanbas/Courses/1234/Assignments/123"
+            href="#/Kanbas/Courses/1234/Assignments/124"
           >
             A2 - Queues and Stacks
           </a>
           <br />
-          Multiple Modules | <b>Not available until</b> June 27 12:00 am |{" "}
+          <span className="text-red" style={{ marginLeft: '35px'}}>Multiple Modules</span> | <b>Not available until</b> June 27 at 12:00 am |{" "}
           <b>Due</b> July 7 at 11:59 pm | 100 pts
+          <LessonControlButtons/>
         </li>
-        <li className="wd-assignment-list-item">
+        <li className="wd-assignment-list-item list-group-item p-3 mb-3 border-gray">
+          <BsGripVertical className="me-2 fs-3" />
           <a
             className="wd-assignment-link"
-            href="#/Kanbas/Courses/1234/Assignments/123"
+            href="#/Kanbas/Courses/1234/Assignments/125"
           >
             A3 - Heaps and Priority Queues
           </a>
           <br />
-          Multiple Modules | <b>Not available until</b> Aug 7 at 12:00 am |{" "}
+          <span className="text-red" style={{ marginLeft: '35px'}}>Multiple Modules</span>  | <b>Not available until</b> Aug 7 at 12:00 am |{" "}
           <b>Due</b> Aug 13 at 11:59 pm | 100 pts
+          <LessonControlButtons/>
         </li>
-        <li className="wd-assignment-list-item">
+        <li className="wd-assignment-list-item list-group-item p-3 mb-3 border-gray">
+          <BsGripVertical className="me-2 fs-3" />
           <a
             className="wd-assignment-link"
-            href="#/Kanbas/Courses/1234/Assignments/123"
+            href="#/Kanbas/Courses/1234/Assignments/126"
           >
-            A4 -Graphs
+            A4 - Graphs
           </a>
           <br />
-          Multiple Modules | <b>Not available until</b> Sept 13 at 12:00 am |{" "}
-          <b>Due</b> Sep 30 at 11:59 pm | 100 pts
+          <span className="text-red" style={{ marginLeft: '35px'}}>Multiple Modules</span>  | <b>Not available until</b> Sept 13 at 12:00 am |{" "}
+          <b>Due</b> Sept 30 at 11:59 pm | 100 pts
+          <LessonControlButtons/>
         </li>
-        <li className="wd-assignment-list-item">
+        <li className="wd-assignment-list-item list-group-item p-3 mb-3 border-gray">
+          <BsGripVertical className="me-2 fs-3" />
           <a
             className="wd-assignment-link"
-            href="#/Kanbas/Courses/1234/Assignments/123"
+            href="#/Kanbas/Courses/1234/Assignments/127"
           >
             A5 - Dynamic Programming
           </a>
           <br />
-          Multiple Modules | <b>Not available until</b> Oct 08 at 12:00 am |{" "}
+          <span className="text-red" style={{ marginLeft: '35px'}}>Multiple Modules</span>  | <b>Not available until</b> Oct 08 at 12:00 am |{" "}
           <b>Due</b> Oct 20 at 11:59 pm | 100 pts
+          <LessonControlButtons/>
         </li>
-        <li className="wd-assignment-list-item">
+        <li className="wd-assignment-list-item list-group-item p-3 mb-3 border-gray">
+          <BsGripVertical className="me-2 fs-3" />
           <a
             className="wd-assignment-link"
-            href="#/Kanbas/Courses/1234/Assignments/123"
+            href="#/Kanbas/Courses/1234/Assignments/128"
           >
             A6 - Advanced Data Structures
           </a>
           <br />
-          Multiple Modules | <b>Not available until</b> Nov 4 at 12:00 am |{" "}
+          <span className="text-red" style={{ marginLeft: '35px'}}>Multiple Modules</span>  | <b>Not available until</b> Nov 4 at 12:00 am |{" "}
           <b>Due</b> Nov 21 at 11:59 pm | 100 pts
+          <LessonControlButtons/>
         </li>
+        
       </ul>
     </div>
   );
