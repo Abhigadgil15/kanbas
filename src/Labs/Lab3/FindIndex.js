d752e1fd00e827bdd4895726d65d4d7fb03f6555
Abhinav Gadgil <64949128+Abhigadgil15@users.noreply.github.com>
2024-10-12 23:53:50 -0400

    index.js update

--- src/Labs/Lab3/FindIndex.js
+++ src/Labs/Lab3/FindIndex.js
@@ -1,6 +1,8 @@
+// data.js
 let numberArray1 = [1, 2, 4, 5, 6];
 let stringArray1 = ['string1', 'string3'];
 
 const fourIndex = numberArray1.findIndex(a => a === 4);
 const string3Index = stringArray1.findIndex(a => a === 'string3');
 
+export { numberArray1, stringArray1, fourIndex, string3Index };
--- src/Labs/Lab3/index.js
+++ src/Labs/Lab3/index.js
@@ -3,8 +3,39 @@ import VariablesAndConstants from './VariablesAndConstants';
 import VariableTypes from "./VariableTypes";
 import IfElse from "./IfElse";
 import Highlight from "./Highlight";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalLoginInline";
+import LegacyFunctions from "./LegacyFunction";
+import ArrowFunctions from "./ArrowFUnctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import * as Data from './FindIndex';
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JSONStringify";
+import House from "./House";
+import TodoList from "./TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import PathParameters from "./PathParameters";
 export default function Lab3() {
     console.log("Hello World!");
+    const combinedData = {
+        numberArray1: Data.numberArray1,
+        stringArray1: Data.stringArray1,
+        fourIndex: Data.fourIndex,
+        string3Index: Data.string3Index,
+      };
     return(
       <div id="wd-lab3">
         <h3>Lab 3</h3>
@@ -12,6 +43,32 @@ export default function Lab3() {
         <VariableTypes/>
         <BooleanVariables/>
         <IfElse/>
+        <ForLoops/>
+        <TernaryOperator/>
+        <ConditionalOutputIfElse/>
+        <ConditionalOutputInline/>
+        <LegacyFunctions/>
+        <ArrowFunctions/>
+        <ImpliedReturn/>
+        <TemplateLiterals/>
+        <SimpleArrays/>
+        <ArrayIndexAndLength/>
+        <AddingAndRemovingToFromArrays/>
+        <MapFunction/>
+        <FindFunction/>
+        <FindIndex/>
+        <FilterFunction/>
+        <JsonStringify/>
+        <House/>
+        <TodoList/>
+        <Spreading/>
+        <Destructing/>
+        <FunctionDestructing/>
+        <Math/>
+        <DestructingImports/>
+        <Classes/>
+        <Styles/>
+        <PathParameters/>
         <h4>Square of 4</h4>
         <Square>4</Square>
         <Highlight>
